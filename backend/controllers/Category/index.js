85ebbe647b7a9069127da22d14ac6b067db7a4b1
Iz0nite <armanddfl@gmail.com>
2024-01-31 09:20:16 +0100

    feat(back): add creatCard route

---
+++ backend/controllers/Category/index.js
@@ -0,0 +1,12 @@
+const CATEGORY = [ 
+  "FIRST",
+  "SECOND",
+  "THIRD",
+  "FOURTH",
+  "FIFTH",
+  "SIXTH",
+  "SEVENTH",
+  "DONE"
+]
+
+module.exports = { CATEGORY }
--- backend/index.js
+++ backend/index.js
@@ -9,10 +9,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/cards", require("./routes/cards"))
 
-app.get("/", (req, res) => {
-  return res.send("API working !")
-})
-
 app.use(function(req, res, next) {
   return res.status(404).json({ error: 'this route doesn\'t exist.' })
 });
